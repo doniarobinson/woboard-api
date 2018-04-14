22f5b2c5ff4659bf3de0da50a221ebb4f72e9f62
doniarobinson <doniamae@gmail.com>
2018-04-14 13:21:15 -0500

    Enable add workout functionality

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -1,8 +1,19 @@
 module Api::V1
   class WorkoutsController < ApplicationController
     def index
-      @workouts = Workout.all
+      @workouts = Workout.order("created_at DESC")
       render json: @workouts
     end
+    
+    def create
+      @workout = Workout.create(workout_params)
+      render json: @workout
+    end
+    
+    private
+    
+      def workout_params
+        params.require(:workout).permit(:title, :worktype, :datetime, :location, :description)
+      end
   end
 end
