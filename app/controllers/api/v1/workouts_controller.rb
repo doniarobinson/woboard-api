7c76c4bea09f32e886510dccb48cc329e24abcde
doniarobinson <doniamae@gmail.com>
2018-04-14 14:15:51 -0500

    Add delete functionality

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -10,6 +10,21 @@ module Api::V1
       render json: @workout
     end
     
+    def update
+      @workout = Workout.find(params[:id])
+      @workout.update_attributes(workout_params)
+      render json: @workout
+    end
+    
+    def destroy
+      @workout = Workout.find(params[:id])
+      if @workout.destroy
+        head :no_content, status: :ok
+      else
+        render json: @workout.errors, status: :unprocessable_entity
+      end
+    end
+    
     private
     
       def workout_params
